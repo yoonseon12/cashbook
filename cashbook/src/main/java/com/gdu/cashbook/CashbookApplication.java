60292880361783a8ddaa0adefb0e1ffc985504e6
GDJ26 <GDJ26@DESKTOP-BCBMNLO>
2020-05-12 11:55:07 +0900

    1. controller추가+index.html추가

--- cashbook/src/main/java/com/gdu/cashbook/CashbookApplication.java
+++ cashbook/src/main/java/com/gdu/cashbook/CashbookApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-// @SpringBootApplication = @Configration + @Enable + @ComponetScan
+// @SpringBootApplication = @Configration + @EnableAutoConfiguration + @ComponetScan
 public class CashbookApplication {
 
 	public static void main(String[] args) {
