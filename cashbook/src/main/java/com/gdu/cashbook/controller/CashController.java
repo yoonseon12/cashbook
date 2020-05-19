4211ea345a3e9db0ea1b737d9c2f4f7cd3df3fc3
GDJ26 <GDJ26@DESKTOP-BCBMNLO>
2020-05-19 18:23:46 +0900

    [이윤선] 원화 컴마 추가 -thymeleaf기능으로 원화 컴마추가 -날짜 연도확인하는 year변수 생성 후 폼 추가

--- cashbook/src/main/java/com/gdu/cashbook/controller/CashController.java
+++ cashbook/src/main/java/com/gdu/cashbook/controller/CashController.java
@@ -1,5 +1,6 @@
 package com.gdu.cashbook.controller;
 
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.Date;
@@ -24,38 +25,43 @@ public class CashController {
 	@Autowired private CashService cashService;
 	@GetMapping("/getCashListByDate")
 	//가계부 출력
-	public String getCashListByDate(HttpSession session, Model model,@RequestParam(value="day", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate day) {
+	public String getCashListByDate(HttpSession session, Model model,@RequestParam(value="date", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date) {
 		if(session.getAttribute("loginMember")==null) { // 로그인 상태 X
 			return "redirect:/index";
 		}
-		if(day == null) {
-			day = LocalDate.now();
+		if(date == null) {
+			date = LocalDate.now();
 		}
-		System.out.println(day+" <- CashController.getCashListByDate: day");
-		model.addAttribute("day", day);
+		System.out.println(date+" <- CashController.getCashListByDate: date");
+		model.addAttribute("date", date);
+		
+		// 연도만 확인할 수 있도록 문자열 자름
+		String stringDate = date.toString();
+		String year = stringDate.substring(stringDate.length()-10, stringDate.length()-6);
+		model.addAttribute("year", year);
+		System.out.println(year+" <- CashController.getCashListByDate: year");
+		
 		// 로그인 아이디
 		String loginMemberId = ((LoginMember)(session.getAttribute("loginMember"))).getMemberId();
 		System.out.println(loginMemberId+" <- CashController.getCashListByDate: loginMemberId");
 		// 오늘 날짜를 구해서 원하는 문자열 형태로 변경
-		
 		Cash cash = new Cash(); // + id, + date(yyyy-mm-dd)
 		cash.setMemberId(loginMemberId);
-		cash.setCashDate(day.toString());
-		
+		cash.setCashDate(date.toString());
 		Map<String, Object> map = cashService.getCashListByDate(cash);
 		model.addAttribute("cashKindSum", map.get("cashKindSum"));
 		model.addAttribute("cashList", map.get("cashList"));
-		System.out.println(map.get("cashKindSum")+"<<<<<<<<<<<<<<<<<<");
 		return "getCashListByDate";
 	}
 	// 가계부 삭제
 	@GetMapping("/removeCash")
-	public String removeCash(HttpSession session, @RequestParam(value="cashNo") int cashNo) { //, @RequestParam(value="") {
+	public String removeCash(HttpSession session, @RequestParam(value="cashNo") int cashNo, @RequestParam(value="date") @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate date) {
 		if(session.getAttribute("loginMember")==null) { // 로그인상태 X
 			return "login";
 		}
 		System.out.println(cashNo+" <- CashController.removeCash: cashNo");
+		System.out.println(date+" <- CashController.removeCash: day");
 		cashService.removeCash(cashNo);
-		return "redirect:/getCashListByDate";
+		return "redirect:/getCashListByDate?day="+date;
 	}
 }
